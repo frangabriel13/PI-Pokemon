5f5417df92355cf803cb9e29c85d209bdac97d4e
franco <mansilla.franco.1@gmail.com>
2023-05-12 15:31:20 -0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Home from './components/Home';
-import PokeCreate from './components/PokeCreate';
-import Detail from './components/Detail';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+import PokeCreate from "./components/PokeCreate";
+import Detail from "./components/Detail";
+import axios from "axios";
+// axios.defaults.baseURL = "http://localhost:3001/"
+axios.defaults.baseURL = "https://pi-pokemon-production-c1a5.up.railway.app/";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path='/' component={LandingPage}/>
-          <Route path='/home' component={Home}/>
-          <Route exact path='/pokemons/:id' component={Detail} />
-          <Route exact path='/pokemons' component={PokeCreate}/>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route exact path="/pokemons/:id" component={Detail} />
+          <Route exact path="/pokemons" component={PokeCreate} />
         </Switch>
       </div>
     </BrowserRouter>
@@ -21,7 +24,6 @@ function App() {
 }
 
 export default App;
-
 
 // import './App.css';
 
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,87 +1,87 @@
-import axios from 'axios';
+import axios from "axios";
 
 export function getPokemons() {
-  return async function(dispatch) {
-    let json = await axios.get('http://localhost:3001/pokemons');
+  return async function (dispatch) {
+    let json = await axios.get("/pokemons");
     // console.log(json)
     return dispatch({
-      type: 'GET_POKEMONS',
-      payload: json.data
-    })
-  }
+      type: "GET_POKEMONS",
+      payload: json.data,
+    });
+  };
 }
 
 export function getTypes() {
-  return async function(dispatch) {
-    let json = await axios.get('http://localhost:3001/types');
+  return async function (dispatch) {
+    let json = await axios.get("/types");
     return dispatch({
-      type: 'GET_TYPES',
-      payload: json.data
-    })
-  }
+      type: "GET_TYPES",
+      payload: json.data,
+    });
+  };
 }
 
 export function postPokemon(payload) {
-  return async function(dispatch) {
-    let response = await axios.post('http://localhost:3001/pokemons', payload);
+  return async function (dispatch) {
+    let response = await axios.post("/pokemons", payload);
     // console.log(response);
     return response;
-  }
+  };
 }
 
 export function filterByType(payload) {
   // console.log(payload);
   return {
-    type: 'FILTER_BY_TYPE',
-    payload
-  }
+    type: "FILTER_BY_TYPE",
+    payload,
+  };
 }
 
 export function filterCreated(payload) {
   return {
-    type: 'FILTER_CREATED',
-    payload
-  }
+    type: "FILTER_CREATED",
+    payload,
+  };
 }
 
 export function orderByName(payload) {
   return {
-    type: 'ORDER_BY_NAME',
-    payload
-  }
+    type: "ORDER_BY_NAME",
+    payload,
+  };
 }
 
 export function orderByAttack(payload) {
   return {
-    type: 'ORDER_BY_ATTACK',
-    payload
-  }
+    type: "ORDER_BY_ATTACK",
+    payload,
+  };
 }
 
 export function getNamePokemon(name) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
-      let json = await axios.get('http://localhost:3001/pokemons?name=' + name);
+      let json = await axios.get("/pokemons?name=" + name);
       return dispatch({
-        type: 'GET_NAME_POKEMON',
-        payload: json.data
-      })
-    } catch(error) {
+        type: "GET_NAME_POKEMON",
+        payload: json.data,
+      });
+    } catch (error) {
       console.log(error);
     }
-  } 
+  };
 }
 
 export function getDetail(id) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
-      let json = await axios.get('http://localhost:3001/pokemons/' + id);
+      let json = await axios.get("/pokemons/" + id);
       return dispatch({
-        type: 'GET_DETAIL',
-        payload: json.data
-      })
-    } catch(error) {
+        type: "GET_DETAIL",
+        payload: json.data,
+      });
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
